5b650b62ac82637435d71eaabad158351c490e65
Wenlin Huang <jackhwl@hotmail.com>
2020-08-14 22:09:50 -0400

    add parameters to removeComments.ps1

--- removeCommends.ps1
+++ removeCommends.ps1
@@ -1,4 +1,8 @@
+#.\removeComment.ps1 "c:\publish\ab.config" "(?ms)^\s*<!--\s*[\r\n]*\s*\*\*\*\*\*[\s\S\n]*?-->[\r\n]*"
+
+$webConfigFilePath=$args[0]
+$pattern=$args[1]
 [IO.Directory]::SetCurrentDirectory((Convert-Path (Get-Location -PSProvider FileSystem)))
-$filetxt = [IO.File]::ReadAllText("c:\publish\ab.config")
-$filetxt = ($filetxt -replace "(?ms)^\s*<!--\s*[\r\n]*\s*\*\*\*\*\*[\s\S\n]*?-->[\r\n]*", "")
-Set-Content -Path "c:\publish\ab.config" -Value $filetxt
+$filetxt = [IO.File]::ReadAllText($webConfigFilePath)
+$filetxt = ($filetxt -replace $pattern, "")
+Set-Content -Path $webConfigFilePath -Value $filetxt
